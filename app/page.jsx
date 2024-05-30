6355120502b897ac4def10031834d2dbb1cd5240
Justin Higgs <justinphiggs@gmail.com>
2024-05-30 14:46:07 -0400

    Added more state to nav components: show/hide profile items and login button on isLoggedIn, but don't have auth set up yet so just placeholder.

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,6 @@ import Link from 'next/link';
 import React from 'react';
 
 const HomePage = () => {
-  console.log('i b when she stroganoff my beef n i wuhh');
-
   return (
     <>
       <h1 className='text-3xl'>Welcome</h1>
