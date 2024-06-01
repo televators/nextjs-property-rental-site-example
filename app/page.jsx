ba6072f8c9e057076ef0e0c75c6f0ecc2ea37bec
Justin Higgs <justinphiggs@gmail.com>
2024-06-01 16:05:12 -0400

    Added new components: hero, infoboxes, infobox, footer. Added hero and footer to main page.

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,14 @@
 'use client';
-import Link from 'next/link';
+import Footer from '@/components/Footer';
+import Hero from '@/components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
 
 const HomePage = () => {
   return (
     <>
-      <h1 className='text-3xl'>Welcome</h1>
-      <Link href='/properties'>Show properties</Link>
+      <Hero />
+      <InfoBoxes></InfoBoxes>
+      <Footer />
     </>
   );
 };
