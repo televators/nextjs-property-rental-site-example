27639ef0289f6792b714c6e2e07a3e8e6abfb39b
Justin Higgs <justinphiggs@gmail.com>
2024-06-01 20:35:42 -0400

    Added properties JSON mock DB as placeholder for UI dev until I get to building out the DB and API properly. Added new PropertyCard component and set up adding markup to Properties page.

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,22 @@
-import Link from 'next/link';
+import PropertyCard from '@/components/PropertyCard';
+import properties from '@/properties.json';
+// import Link from 'next/link';
 
 const PropertiesPage = () => {
   return (
-    <>
-      <h1 className="text-3xl">Properties</h1>
-      <Link href='/'>Go Home</Link>
-    </>
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        { properties.length === 0 ? (
+          <h2>No properties found.</h2>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            { properties.map( ( property ) => (
+              <PropertyCard key={ property._id } property={ property } />
+            ) ) }
+          </div>
+        ) }
+      </div>
+    </section>
   );
 };
 
