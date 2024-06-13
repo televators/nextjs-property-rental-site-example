84f1672670bb7f5925be7e2d26d7b358d652d560
Justin Higgs <justinphiggs@gmail.com>
2024-06-13 14:43:00 -0400

    Changed the route for all bookmarks to use the same language as the rest of the bookmark UI/functionality, i.e., calling the route/page Bookmarks instead of Saved. Also added the ability to remove a bookmark from the Bookmarks page and have the PropertyCard accept a bool for showing the remove button if on the Bookmarks page. It triggers a refresh since the page is a server component, currently.

---
+++ app/properties/bookmarked/page.jsx
@@ -0,0 +1,41 @@
+import connectDB from '@/config/database';
+import User from '@/models/User';
+import { getSessionUser } from '@/utils/getSessionUser';
+import PropertyCard from '@/components/PropertyCard';
+import { convertToSerializableObject } from '@/utils/convertToObject';
+
+const BookmarkedProperties = async () => {
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userID) {
+    return <h1>You must sign in to see property bookmarks.</h1>;
+  }
+
+  const { bookmarks: bookmarkEntries } = await User.findById(sessionUser.userID).populate('bookmarks').lean();
+  const bookmarkObjects = [];
+
+  bookmarkEntries.map((bookmark) => {
+    const bookmarkObject = convertToSerializableObject(bookmark);
+    bookmarkObjects.push(bookmarkObject);
+  });
+
+  return (
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        <h1 className='text-2xl mb-4'>Your Bookmarked Properties</h1>
+        {bookmarkObjects.length === 0 ? (
+          <p>No bookmarked properties.</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {bookmarkObjects.map((property) => (
+              <PropertyCard key={property._id} property={property} enableBookmarkToggle={true} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+export default BookmarkedProperties;
--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -1,11 +1,18 @@
-import Image from "next/image";
-import Link from "next/link";
-import { FaBed, FaBath, FaRulerCombined, FaMoneyBill, FaMapMarker } from "react-icons/fa";
+'use client';
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { toast } from 'react-toastify';
+import { FaBed, FaBath, FaRulerCombined, FaMoneyBill, FaMapMarker, FaBookmark } from 'react-icons/fa';
 
-const PropertyCard = ( { property } ) => {
+// enableBookmarkToggle is only used on the user's All Bookmarks page at /properties/bookmarks
+const PropertyCard = ({ property, enableBookmarkToggle }) => {
+  const { rates, _id: propertyID } = property;
+
+  // This is used on the All Bookmarks page for signed-in users. Users can delete properties from the page but since the page is a server component
+  const [wasRemoved, setWasRemoved] = useState(false);
+
   const getRateDisplay = () => {
-    const { rates } = property;
-
     //#region Perf Prof Maybz
     // TODO: Profile this basic usage of Number.prototype.toLocaleString() against Intl.NumberFormat for this setup. Curious how much of an impact it is. See about generating 90 more properties for performance test.
     // return new Intl.NumberFormat( 'en-US', {
@@ -15,84 +22,116 @@ const PropertyCard = ( { property } ) => {
     // } ).format( rates.nightly );
     //#endregion
 
-    if ( rates.monthly ) {
-      return `${ '$' + rates.monthly.toLocaleString() }/mo`;
-    } else if ( rates.weekly ) {
-      return `${ '$' + rates.weekly.toLocaleString() }/wk`;
-    } else if ( rates.nightly ) {
-      return `${ '$' + rates.nightly.toLocaleString() }/night`;
+    if (rates.monthly) {
+      return `${'$' + rates.monthly.toLocaleString()}/mo`;
+    } else if (rates.weekly) {
+      return `${'$' + rates.weekly.toLocaleString()}/wk`;
+    } else if (rates.nightly) {
+      return `${'$' + rates.nightly.toLocaleString()}/night`;
     }
   };
 
-  // DEBUG:
-  // console.log( property.images );
+  const handleClick = async () => {
+    try {
+      const res = await fetch(`/api/bookmarks/${propertyID}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          propertyId: propertyID,
+        }),
+      });
+
+      if (res.status === 200) {
+        const data = await res.json();
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error('Error trying to remove bookmarked property.');
+    }
+  };
 
   return (
-    <div className="rounded-xl shadow-md relative">
+    <div className='rounded-xl shadow-md relative'>
       <Image
         className='w-full h-auto rounded-t-xl aspect-[3/2] object-cover'
-        src={ property.images[0] }
-        alt=""
+        src={property.images[0]}
+        alt=''
         width={0}
         height={0}
-        sizes="100vw"
-        priority={ true }
+        sizes='100vw'
+        priority={true}
       />
 
-      <div className="p-4">
-        <div className="text-left md:text-center lg:text-left mb-6">
-          <div className="text-gray-600">{ property.type }</div>
-          <h3 className="text-xl font-bold">{ property.name }</h3>
+      <div className='p-4'>
+        <div className='text-left md:text-center lg:text-left mb-6'>
+          <div className='text-gray-600'>{property.type}</div>
+          <h3 className='text-xl font-bold mb-0.5'>{property.name}</h3>
+
+          <div className='flex align-middle gap-2 mb-4 lg:mb-0'>
+            <FaMapMarker className='inline text-lg text-orange-700 mt-0.5' />
+            <span className='text-orange-700'>{property.location.city + ', ' + property.location.state}</span>
+          </div>
         </div>
 
-        <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
-          { getRateDisplay() }
+        <h3 className='absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right'>
+          {getRateDisplay()}
         </h3>
 
-        <div className="flex justify-center gap-4 text-gray-500 mb-4">
+        <div className='flex justify-center gap-4 text-gray-500 mb-4'>
           <p>
-            <FaBed className="inline mr-1" /> { property.beds }
-            <span className="md:hidden lg:inline">Beds</span>
+            <FaBed className='inline mr-1' /> {property.beds}
+            <span className='md:hidden lg:inline'>Beds</span>
           </p>
           <p>
-            <FaBath className="inline mr-1" /> { property.baths }
-            <span className="md:hidden lg:inline">Baths</span>
+            <FaBath className='inline mr-1' /> {property.baths}
+            <span className='md:hidden lg:inline'>Baths</span>
           </p>
           <p>
-            <FaRulerCombined className="inline mr-1" />
-            { property.square_feet } <span className="md:hidden lg:inline">sqft</span>
+            <FaRulerCombined className='inline mr-1' />
+            {property.square_feet} <span className='md:hidden lg:inline'>sqft</span>
           </p>
         </div>
 
-        <div className="flex justify-center gap-4 text-green-900 text-sm mb-4">
-          { property.rates.nightly && (
-            <p><FaMoneyBill className="inline mr-0.5 mb-0.5" /> Nightly</p>
-          ) }
-          { property.rates.weekly && (
-            <p><FaMoneyBill className="inline mr-0.5 mb-0.5" /> Weekly</p>
-          ) }
-          { property.rates.monthly && (
-            <p><FaMoneyBill className="inline mr-0.5 mb-0.5" /> Monthly</p>
-          ) }
+        <div className='flex justify-center gap-4 text-green-900 text-sm mb-4'>
+          {property.rates.nightly && (
+            <p>
+              <FaMoneyBill className='inline mr-0.5 mb-0.5' /> Nightly
+            </p>
+          )}
+          {property.rates.weekly && (
+            <p>
+              <FaMoneyBill className='inline mr-0.5 mb-0.5' /> Weekly
+            </p>
+          )}
+          {property.rates.monthly && (
+            <p>
+              <FaMoneyBill className='inline mr-0.5 mb-0.5' /> Monthly
+            </p>
+          )}
         </div>
 
-        <div className="border border-gray-100 mb-5"></div>
+        <div className='border border-gray-100 mb-5'></div>
 
-        <div className="flex flex-col lg:flex-row justify-between mb-4">
-          <div className="flex align-middle gap-2 mb-4 lg:mb-0">
-            <FaMapMarker className="inline text-lg text-orange-700 mt-1"/>
-            <span className="text-orange-700">{ property.location.city + ', ' + property.location.state }</span>
-          </div>
-
+        <div className='flex flex-col lg:flex-row justify-end gap-3 mb-4'>
           <Link
-            href={ `/properties/${ property._id }` }
-            className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-          >
+            href={`/properties/${property._id}`}
+            className='h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
             Details
           </Link>
+
+          {enableBookmarkToggle && (
+            <button
+              onClick={handleClick}
+              className={`bg-red-500 hover:bg-red-600 text-white text-sm font-bold py-2 px-4 rounded-lg flex items-center justify-center`}>
+              <FaBookmark className='mr-2' /> Remove
+            </button>
+          )}
         </div>
       </div>
     </div>
   );
-}
+};
 export default PropertyCard;
