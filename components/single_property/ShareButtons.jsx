780df403e9eebb122bdd08c25f03bff08ef83814
Justin Higgs <justinphiggs@gmail.com>
2024-06-15 16:23:04 -0400

    Added react-share and set up share buttons on Single Property page.

--- components/single_property/ShareButtons.jsx
+++ components/single_property/ShareButtons.jsx
@@ -1,10 +1,44 @@
-import { FaShare } from 'react-icons/fa';
+'use client';
+
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  EmailShareButton,
+  EmailIcon,
+} from 'react-share';
 
 const ShareButtons = ({ property }) => {
+  const shareURL = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`;
+  const propertyType = property.type.replace(/\s/g, '');
+
   return (
-    <button className='bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
-      <FaShare className='mr-2' /> Share Property
-    </button>
+    <>
+      <h3 className='text-xl font-bold text-center pt-2'>Share this property:</h3>
+      <div className='flex gap-3 justify-center pb-4'>
+        <FacebookShareButton url={shareURL} quote={property.name} hashtag={`#${propertyType}ForRent`}>
+          <FacebookIcon size={40} round={true} />
+        </FacebookShareButton>
+
+        <TwitterShareButton url={shareURL} title={property.name} hashtags={[`#${propertyType}ForRent`]}>
+          <TwitterIcon size={40} round={true} />
+        </TwitterShareButton>
+
+        <WhatsappShareButton url={shareURL} title={property.name} separator=':: '>
+          <WhatsappIcon size={40} round={true} />
+        </WhatsappShareButton>
+
+        <EmailShareButton
+          url={shareURL}
+          subject={property.name}
+          body='Check out this property listing on PropertyPulse'>
+          <EmailIcon size={40} round={true} />
+        </EmailShareButton>
+      </div>
+    </>
   );
 };
 export default ShareButtons;
